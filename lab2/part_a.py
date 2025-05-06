8879e72fd8a046147c8465261f75f0083a07f3df
Maxwell Jung <jungmaxwell@gmail.com>
2025-05-06 14:33:11 -0700

    confirm weight stationary example

--- lab2/part_a.py
+++ lab2/part_a.py
@@ -3,10 +3,15 @@ from system_specs import *
 
 def main():
     # Calculate WS energy for 6th layer, i.e. Conv [512, 256, 3, 3]
-    ws_total_energy = ws_conv_energy(
+    print(f'{example_ws_conv_energy(
         input_width=16, input_height=16,
         num_filter=512, num_channel=256, filter_width=3, filter_height=3,
-        batch_size=1)
+        batch_size=1)=}')
+
+    print(f'{calc_weight_stationary_conv_energy(
+        input_width=16, input_height=16,
+        num_filter=512, num_channel=256, filter_width=3, filter_height=3,
+        batch_size=1)=}')
     
     test()
 
@@ -169,7 +174,7 @@ def calc_output_stationary_matrix_mult_energy(weight_rows=32, input_rows=27, inp
     return total_energy
 
 
-def ws_conv_energy(input_width=32, input_height=32,
+def example_ws_conv_energy(input_width=32, input_height=32,
                    num_filter=32, num_channel=3, filter_width=3, filter_height=3, 
                    batch_size=1):
 
@@ -190,6 +195,51 @@ def ws_conv_energy(input_width=32, input_height=32,
     output_sram_energy = output_sram_write_energy + output_sram_read_energy
 
     dot_product_energy = num_dot_prod_cycles * NUM_DPU * SINGLE_DPU_ENERGY_PER_CYCLE
+
+    total_energy = weight_sram_energy + input_sram_energy + output_sram_energy + dot_product_energy
+
+    return total_energy
+
+
+def calc_weight_stationary_conv_energy(
+        input_width=32, input_height=32,
+        num_filter=32, num_channel=3, filter_width=3, filter_height=3, 
+        batch_size=1):
+    
+    # Convert convolution to matrix-matrix multiply, i.e. WI = O where
+    # shape(W) = num_filter x (num_channel x filter_width x filter_height) = 32 x (3 x 3 x 3) = 32 x 27
+    # shape(I) = (num_channel x filter_width x filter_height) x (input_width x input_height) = (3 x 3 x 3) x (32 x 32) = 27 x 1024
+    # shape(O) = 32 x 1024
+    # See lecture 2 slide 6 for illustration of im2col
+    return calc_weight_stationary_matrix_mult_energy(weight_rows=num_filter, input_rows=num_channel*filter_width*filter_height, input_cols=input_width*input_height*batch_size)
+
+
+def calc_weight_stationary_matrix_mult_energy(weight_rows=32, input_rows=27, input_cols=1024):
+
+    # Matrix multiply WI = (32 x 27) (27 x 1024) = (32 x 1024)
+    # Note that compute module is limited to Av = (16 x 128) (128 x 1) every cycle
+
+    weight_buf_update_count = math.ceil(weight_rows/NUM_DPU)*math.ceil(input_rows/DPU_SIZE)
+    input_buf_update_per_weight_buf_update = input_cols
+
+    dot_prod_cycle_count = input_buf_update_per_weight_buf_update*weight_buf_update_count
+    dot_product_energy = SINGLE_DPU_ENERGY_PER_CYCLE * dot_prod_cycle_count * NUM_DPU
+
+    # number of weights in weight buffer
+    weight_buf_size = NUM_DPU * DPU_SIZE
+    weight_sram_energy = SRAM_ACC_ENERGY_PER_BIT * (BITS_PER_WEIGHT * weight_buf_size) * weight_buf_update_count
+
+    input_buf_update_count = input_buf_update_per_weight_buf_update*weight_buf_update_count
+    input_buf_size = DPU_SIZE
+    input_sram_energy = SRAM_ACC_ENERGY_PER_BIT * (BITS_PER_INPUT * input_buf_size) * input_buf_update_count
+
+    output_buf_size = NUM_DPU
+    # Write every dot output to SRAM
+    output_buf_update_count = dot_prod_cycle_count
+    output_sram_write_energy = SRAM_ACC_ENERGY_PER_BIT * (BITS_PER_OUTPUT * output_buf_size) * output_buf_update_count
+    # Last partial output does not need to be read from SRAM
+    output_sram_read_energy = ((math.ceil(input_rows/DPU_SIZE)-1)/math.ceil(input_rows/DPU_SIZE)) * output_sram_write_energy
+    output_sram_energy = output_sram_write_energy + output_sram_read_energy
 
     total_energy = weight_sram_energy + input_sram_energy + output_sram_energy + dot_product_energy
 
